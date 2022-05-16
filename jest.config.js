4ab7012a4d4c9bedb33137cfed4c570ddbc2ddf8
MGonza20 <pag20634@uvg.edu.gt>
2022-05-15 23:06:18 -0600

    Generando archivo de configuracion Jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+
+  collectCoverage: true,
+
+  coverageDirectory: 'coverage',
+};
